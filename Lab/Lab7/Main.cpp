df38fd2f38e786d56b3c48e683aaae2a84d7cd71
Jamie <c00208743@itcarlow.ie>
2019-03-14 10:35:54 +0000

    n processes Peterson

--- Lab/Lab7/Main.cpp
+++ Lab/Lab7/Main.cpp
@@ -1,46 +1,48 @@
 #include <thread>
 #include <iostream>
-#include <queue>
-#include <mutex>
+#include <atomic>
+#include <memory>
+#include <string>
+#include <vector>
 
-//bool flag[2] = {false, false};
-//int last = 1;
 
-const int n = 2; //number of processes = n
-int in[n] = { 0,0 };
-int last[n] = { 0,0 };
+const int n = 8;
+int last[n], in[n];
+std::vector<std::thread> threads[n];
 
-void cs(void *s) {
-	
-	while (true) {
-		int n_processes = (int)s;
-		//for every number up to n 
-		for (int j = 0; j < n_processes; j++) {
-			in[n_processes] = j;
-			last[j] = n_processes;
-			//[ k = 1 to n stage i !=k]
-			for (int k = 1; k <= j; k++) {
-				if (k > n_processes) {
-					while (in[k] >= in[n_processes] && last[j] == n_processes) {
-						//busy wait
+void process(int i)
+{
+
+	while (true)
+	{
+		for (int j = 0; j < n; j++)
+		{
+			in[i] = j; last[j] = i;
+			for (int k = 0; k < n; k++)
+			{
+				if (i != k)
+				{
+					while (in[k] >= in[i] && last[j] == i)
+					{
+						continue;
 					}
 				}
 			}
-			//critical section
-			std::cout<< n_processes << "\n";
-			in[n_processes] = 0; //exit protocol
 		}
+		std::cout << "process " + std::to_string(i) << std::endl;
+		in[i] = -1;
 	}
 }
 
 int main()
 {
-	
 
-	std::thread t1(cs,(void*)1);
-	std::thread t2(cs,(void*)2);
-	t1.join();
-	t2.join();
-	std::cout << "finished!" << std::endl;
-	std::cin.get();
+	for (int i = 0; i < n; i++)
+	{
+		threads[i].push_back(std::thread(process, i));
+	}
+	for (int i = 0; i < n; i++)
+	{
+		threads->at(i).join();
+	}
 }
